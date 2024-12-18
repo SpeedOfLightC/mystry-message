a33ac24c7d91c9475441fa303f31e0549cad83c0
Arnab Ghosh <arnab.ghosh.it2021@kgec.ac.in>
2024-12-19 00:00:30 +0530

    Add Navbar component and refactor layout structure; implement MessageCard with delete functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster"
 import Navbar from "@/components/Navbar";
+import './globals.css'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +27,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        {children}
-      </body>
+      <AuthProvider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+          <Toaster />
+        </body>
+      </AuthProvider>
+
     </html>
   );
 }
